9de963f0bfab5719e93873b6adb2493df5fc7674
chanlee4me <cli1004@163.com>
2025-04-13 14:48:08 +0800

    LeetCode279

---
+++ src/main/java/com/leetcode/LeetCode279.java
@@ -0,0 +1,24 @@
+package com.leetcode;
+
+import java.util.Arrays;
+
+public class LeetCode279 {
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            dp[i] = i;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i - j * j] + 1, dp[i]);
+            }
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 12;
+        int res = new LeetCode279().numSquares(n);
+        System.out.println("res = " + res);
+    }
+}
