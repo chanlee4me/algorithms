47bf0489f9aea0bdfc58d63e7a3a7244c5fc7000
chanlee4me <cli1004@163.com>
2025-05-09 11:41:42 +0800

    洛谷入门1-P5705

---
+++ src/main/java/com/luogu/P5705.java
@@ -0,0 +1,16 @@
+package com.luogu;
+
+import java.util.Scanner;
+
+public class P5705 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s1 = in.nextLine();
+        String s2 = "";
+        for (int i = s1.length() - 1; i >= 0; i--) {
+            s2 += s1.charAt(i);
+        }
+        double v = Double.parseDouble(s2);
+        System.out.print(v);
+    }
+}
