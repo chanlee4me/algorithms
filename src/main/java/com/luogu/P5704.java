290f4ac3452d898cfd428758a047d1d52cdd95b9
chanlee4me <cli1004@163.com>
2025-05-09 11:41:39 +0800

    洛谷入门1-P5704

---
+++ src/main/java/com/luogu/P5704.java
@@ -0,0 +1,14 @@
+package com.luogu;
+
+import java.util.Scanner;
+
+public class P5704 {
+    public static void main(String[] args) {
+        int diff = 'A' - 'a';
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        char c = s.charAt(0);
+        c += diff;
+        System.out.println(c);
+    }
+}
