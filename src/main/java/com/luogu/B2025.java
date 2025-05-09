83b381c96b0b8a74b2796fdf2e48d9585ec9d5b2
chanlee4me <cli1004@163.com>
2025-05-09 11:41:22 +0800

    洛谷入门1-B2025

---
+++ src/main/java/com/luogu/B2025.java
@@ -0,0 +1,16 @@
+package com.luogu;
+
+class B2025 {
+    public static void main(String[] args) {
+        String l1 = "  *";
+        String l2 = " ***";
+        String l3 = "*****";
+        String l4 = " ***";
+        String l5 = "  *";
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(l3);
+        System.out.println(l4);
+        System.out.print(l5);
+    }
+}
